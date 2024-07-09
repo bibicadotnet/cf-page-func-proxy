0af1994c793e628f7adcbd088863bb048df210ac
bibica <119997727+bibicadotnet@users.noreply.github.com>
2024-07-09 20:35:48 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,33 @@
-export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('')) {
-        url.hostname="res.cloudinary.com/xinclub/f_webp,q_auto:best/img/";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
-      // Otherwise, serve the static assets.
-      return env.ASSETS.fetch(request);
+const API_HOST = "res.cloudinary.com/xinclub/f_webp,q_auto:best/img/"
+
+async function handleRequest(event) {
+    const url = new URL(event.request.url)
+    const pathname = url.pathname
+    const search = url.search
+    const pathWithParams = pathname + search
+    if (pathname.startsWith("/static/")) {
+        return retrieveStatic(event, pathWithParams)
+    } else {
+        return forwardRequest(event, pathWithParams)
     }
-  };
+}
+
+async function retrieveStatic(event, pathname) {
+    let response = await caches.default.match(event.request)
+    if (!response) {
+        response = await fetch(`https://${API_HOST}${pathname}`)
+        event.waitUntil(caches.default.put(event.request, response.clone()))
+    }
+    return response
+}
+
+async function forwardRequest(event, pathWithSearch) {
+    const request = new Request(event.request)
+    request.headers.delete("cookie")
+    return await fetch(`https://${API_HOST}${pathWithSearch}`, request)
+}
+
+addEventListener("fetch", (event) => {
+    event.passThroughOnException()
+    event.respondWith(handleRequest(event))
+})
