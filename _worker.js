c6ff23b1c44c60582d7c88fe13252c4865f9b481
bibica <119997727+bibicadotnet@users.noreply.github.com>
2024-07-09 20:16:07 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 export default {
     async fetch(request, env) {
       let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="res.cloudinary.com/xinclub/f_webp,q_auto:best/img/";
+      if (url.pathname.startsWith('/xinclub/f_webp,q_auto:best/img/')) {
+        url.hostname="res.cloudinary.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
