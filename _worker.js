67482e21dff9bcb74927ca3947f3ee1b44f79df1
bibica <119997727+bibicadotnet@users.noreply.github.com>
2024-07-09 20:39:12 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,29 +5,4 @@ async function handleRequest(event) {
     const pathname = url.pathname
     const search = url.search
     const pathWithParams = pathname + search
-    if (pathname.startsWith("/static/")) {
-        return retrieveStatic(event, pathWithParams)
-    } else {
-        return forwardRequest(event, pathWithParams)
-    }
 }
-
-async function retrieveStatic(event, pathname) {
-    let response = await caches.default.match(event.request)
-    if (!response) {
-        response = await fetch(`https://${API_HOST}${pathname}`)
-        event.waitUntil(caches.default.put(event.request, response.clone()))
-    }
-    return response
-}
-
-async function forwardRequest(event, pathWithSearch) {
-    const request = new Request(event.request)
-    request.headers.delete("cookie")
-    return await fetch(`https://${API_HOST}${pathWithSearch}`, request)
-}
-
-addEventListener("fetch", (event) => {
-    event.passThroughOnException()
-    event.respondWith(handleRequest(event))
-})
