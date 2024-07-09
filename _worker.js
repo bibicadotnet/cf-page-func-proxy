6002637c53fe741ca97a0e68d74616710dfce315
bibica <119997727+bibicadotnet@users.noreply.github.com>
2024-07-09 20:41:07 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,12 @@
-const API_HOST = "res.cloudinary.com/xinclub/f_webp,q_auto:best/img/"
-
-async function handleRequest(event) {
-    const url = new URL(event.request.url)
-    const pathname = url.pathname
-    const search = url.search
-    const pathWithParams = pathname + search
-}
+export default {
+    async fetch(request, env) {
+      let url = new URL(request.url);
+      if (url.pathname.startsWith('/')) {
+        url.hostname="res.cloudinary.com/xinclub/f_webp,q_auto:best/img/";
+        let new_request=new Request(url,request);
+        return fetch(new_request);
+      }
+      // Otherwise, serve the static assets.
+      return env.ASSETS.fetch(request);
+    }
+  };
