f66db01dd9d0584c88335c096c8aefea4deb162b
bibica <119997727+bibicadotnet@users.noreply.github.com>
2024-07-11 02:02:47 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,60 +1,12 @@
 export default {
-    async fetch(request) {
-        // 处理请求体
-        async function handleRequestBody(request) {
-            if (request.method === "GET" || request.method === "HEAD") {
-                return null;
-            }
-            try {
-                return await request.text();
-            } catch (error) {
-                console.error("Error reading request body:", error);
-                return null;
-            }
-        }
-
-        // 主要的请求处理逻辑
-        async function handleRequest(request) {
-            let url = new URL(request.url);
-            url.hostname = 'res.cloudinary.com/xinclub/f_webp,q_auto:best/img/';
-            url.protocol = 'https:';
-
-            let newRequestHeaders = new Headers(request.headers);
-            newRequestHeaders.set('Host', url.hostname);
-            newRequestHeaders.set('X-Real-IP', request.headers.get('cf-connecting-ip'));
-            newRequestHeaders.set('X-Forwarded-For', request.headers.get('cf-ipcountry'));
-            newRequestHeaders.set('REMOTE-HOST', request.headers.get('cf-connecting-ip'));
-            newRequestHeaders.set('Referer', url.toString());
-
-            let requestBody = await handleRequestBody(request);
-
-            let newRequest = new Request(url, {
-                method: request.method,
-                headers: newRequestHeaders,
-                body: requestBody
-            });
-
-            try {
-                let response = await fetch(newRequest);
-
-                if (response.headers.get('Content-Type')?.includes('text')) {
-                    let text = await response.text();
-                    text = text.replace(/www.example.com/g, 'proxy.example.com');
-
-                    return new Response(text, {
-                        status: response.status,
-                        statusText: response.statusText,
-                        headers: response.headers
-                    });
-                }
-
-                return response;
-            } catch (error) {
-                console.error("Error fetching new request:", error);
-                return new Response("Internal Server Error", { status: 500 });
-            }
-        }
-
-        return handleRequest(request);
+    async fetch(request, env) {
+      let url = new URL(request.url);
+      if (url.pathname.startsWith('/xinclub/f_webp,q_auto:best/img/')) {
+        url.hostname="res.cloudinary.com";
+        let new_request=new Request(url,request);
+        return fetch(new_request);
+      }
+      // Otherwise, serve the static assets.
+      return env.ASSETS.fetch(request);
     }
-};
+  };
